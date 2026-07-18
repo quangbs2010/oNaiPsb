b8dce2697eeb3ed20b9388d1637a273abfb7b458
quangbs2010 <di3686585@163.com>
2026-07-18 18:56:04 +0800

    字典添加redis支持

--- easy_trans_service/src/main/java/com/fhs/trans/service/impl/DictionaryTransService.java
+++ easy_trans_service/src/main/java/com/fhs/trans/service/impl/DictionaryTransService.java
@@ -1,6 +1,7 @@
 package com.fhs.trans.service.impl;
 
 import com.fhs.cache.service.FuncGetter;
+import com.fhs.cache.service.RedisCacheService;
 import com.fhs.common.constant.Constant;
 import com.fhs.common.spring.SpringContextUtil;
 import com.fhs.common.utils.StringUtil;
@@ -13,6 +14,7 @@ import com.fhs.trans.fi.LocaleGetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -31,6 +33,14 @@ public class DictionaryTransService implements ITransTypeService, InitializingBe
     private static final Logger LOGGER = LoggerFactory.getLogger(DictionaryTransService.class);
 
     /**
+     * redis key前缀
+     */
+    private static final String TRANS_PRE = "trans:";
+
+    @Autowired(required = false)
+    private RedisCacheService<String> redisCacheService;
+
+    /**
      * 用来放字典缓存的map
      */
     private Map<String, String> dictionaryTransMap = new ConcurrentHashMap<>();
@@ -40,9 +50,21 @@ public class DictionaryTransService implements ITransTypeService, InitializingBe
      */
     private Map<String, String> unTransMap = new ConcurrentHashMap<>();
 
+    /**
+     * 是否开启国际化
+     */
     private boolean isOpenI18n;
 
+
+    /**
+     * 当前国际化语言环境获取
+     */
     private LocaleGetter localeGetter;
+
+    /**
+     * 是否使用redis 默认false
+     */
+    private boolean isUseRedis;
 
 
     /**
@@ -53,7 +75,11 @@ public class DictionaryTransService implements ITransTypeService, InitializingBe
      */
     public void refreshCache(String dictGroupCode, Map<String, String> dicMap) {
         dicMap.keySet().forEach(dictCode -> {
-            dictionaryTransMap.put(dictGroupCode + "_" + dictCode, dicMap.get(dictCode));
+            if (!isUseRedis) {
+                dictionaryTransMap.put(dictGroupCode + "_" + dictCode, dicMap.get(dictCode));
+            } else {
+                redisCacheService.put(TRANS_PRE + dictGroupCode + "_" + dictCode, dicMap.get(dictCode));
+            }
             unTransMap.put(dictGroupCode + "_" + dicMap.get(dictCode), dictCode);
         });
     }
@@ -76,23 +102,14 @@ public class DictionaryTransService implements ITransTypeService, InitializingBe
             List<String> dicCodeList = new ArrayList<>();
             for (String dicCode : dicCodeArray) {
                 if (!StringUtil.isEmpty(dicCode)) {
-                    dicCodeList.add(dictionaryTransMap.get(getMapKey(key, dicCode)));
+                    if (!isUseRedis) {
+                        dicCodeList.add(dictionaryTransMap.get(getMapKey(key, dicCode)));
+                    } else {
+                        dicCodeList.add(redisCacheService.get(TRANS_PRE + getMapKey(key, dicCode)));
+                    }
                 }
             }
-
-            String transResult = null;
-            //customeBeanFuncName有值的话 调用用户自定义的对应的接口
-            if (!StringUtil.isEmpty(tempTrans.customeBeanFuncName())) {
-                final Object bean = SpringContextUtil.getBean(tempTrans.customeBeanFuncName());
-                if (bean == null || !(bean instanceof FuncGetter)) {
-                    throw new ParamException("无法找到正确的bean:" + tempTrans.customeBeanFuncName());
-                }
-                FuncGetter funcGetter = (FuncGetter) bean;
-                //obj 传进去  这个翻译的值可能依赖这个对象的某些上下文信息 或其他字段 更灵活
-                transResult = funcGetter.get(tempTrans, dicCodes, obj);
-            } else {
-                transResult = dicCodeList.size() > Constant.ZERO ? StringUtil.getStrForIn(dicCodeList, false) : "";
-            }
+            String transResult = dicCodeList.size() > Constant.ZERO ? StringUtil.getStrForIn(dicCodeList, false) : "";
             if (obj.getTransMap() != null) {
                 obj.getTransMap().put(tempField.getName() + "Name", transResult);
             }
@@ -137,6 +154,16 @@ public class DictionaryTransService implements ITransTypeService, InitializingBe
     public void openI18n(LocaleGetter localeGetter) {
         this.isOpenI18n = true;
         this.localeGetter = localeGetter;
+    }
+
+    /**
+     * 标记使用redis
+     */
+    public void makeUseRedis() {
+        this.isUseRedis = true;
+        if (redisCacheService == null) {
+            throw new RuntimeException("使用redis 请将 easy-trans.is-enable-redis 设置为true");
+        }
     }
 
     public Map<String, String> getUnTransMap() {
